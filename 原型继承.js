54113c099cc5f877e66f8c8accb94898e124f174
mccheung <mc_cheung@126.com>
2016-10-13 22:16:27 +0800

    update

--- 原型继承.js
+++ 原型继承.js
@@ -13,3 +13,17 @@ var a = new Person('sven');
 console.log( a.name );
 console.log( a.getName() );
 console.log( Person.prototype === Object.getPrototypeOf(a) );
+
+
+
+var objectFactory = function() {
+  var obj = new Object, Constructor = [].shift.call( arguments );
+
+  obj.__proto__ = Constructor.prototype;
+  var ret = Constructor.apply( obj, arguments );
+
+  return typeof ret === 'object' ? ret : obj;
+};
+
+
+var b = objectFactory( Person, 'sven' );
