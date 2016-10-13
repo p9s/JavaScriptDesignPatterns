eb3e69cf2710b32e1772cff4cb3225bf65909041
mccheung <mc_cheung@126.com>
2016-10-13 22:40:16 +0800

    update

--- 原型继承.js
+++ 原型继承.js
@@ -1,29 +1,38 @@
-// WebStom 可以正常运行
+// // WebStom 可以正常运行
 
-function Person(name) {
-    this.name = name;
-};
+// function Person(name) {
+//     this.name = name;
+// };
 
 
-Person.prototype.getName = function () {
-    return this.name;
-};
+// Person.prototype.getName = function () {
+//     return this.name;
+// };
 
-var a = new Person('sven');
+// var a = new Person('sven');
+// console.log( a.name );
+// console.log( a.getName() );
+// console.log( Person.prototype === Object.getPrototypeOf(a) );
+
+
+
+// var objectFactory = function() {
+//   var obj = new Object, Constructor = [].shift.call( arguments );
+
+//   obj.__proto__ = Constructor.prototype;
+//   var ret = Constructor.apply( obj, arguments );
+
+//   return typeof ret === 'object' ? ret : obj;
+// };
+
+
+// var b = objectFactory( Person, 'sven' );
+
+
+var obj = { name: 'sven' };
+var A = function(){};
+
+A.prototype = obj;
+
+var a = new A();
 console.log( a.name );
-console.log( a.getName() );
-console.log( Person.prototype === Object.getPrototypeOf(a) );
-
-
-
-var objectFactory = function() {
-  var obj = new Object, Constructor = [].shift.call( arguments );
-
-  obj.__proto__ = Constructor.prototype;
-  var ret = Constructor.apply( obj, arguments );
-
-  return typeof ret === 'object' ? ret : obj;
-};
-
-
-var b = objectFactory( Person, 'sven' );
